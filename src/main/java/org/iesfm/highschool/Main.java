e5794e957eb91335ae3e2e1a6e1e06a264db15a7
AngelClase <angel.torijasanchezj@iesfm.onmicrosoft.com>
2022-10-26 10:36:39 +0200

    corregido

--- src/main/java/org/iesfm/highschool/Main.java
+++ src/main/java/org/iesfm/highschool/Main.java
@@ -8,15 +8,18 @@ import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+
 public class Main {
     private final static Logger log = LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) {
+
             ApplicationContext context = new AnnotationConfigApplicationContext(
                     HighschoolConfiguration.class
             );
 
             HighschoolProgram program = context.getBean(HighschoolProgram.class);
             program.execute();
+
         }
     }
